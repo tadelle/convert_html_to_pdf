cc2cdb748df217f9f262d700e23fc6beae8eb1e8
José Carlos Tadelle Jr. <jose.tadelle@gmail.com>
2024-03-23 20:11:19 +0000

    Alteração da versão da biblioteca xhtml2pdf.

--- src/service/bucket.py
+++ src/service/bucket.py
@@ -1,4 +1,3 @@
-#import os
 import uuid
 import boto3
 import typing
@@ -8,13 +7,14 @@ if typing.TYPE_CHECKING:
 
 s3: "S3Client" = boto3.client("s3")
 
-def download_file(bucket_name, key):
+
+def download_file(bucket_name: str, key: str) -> str:
     tmpkey = key.replace('/', '')
     download_path = f'/tmp/{uuid.uuid4()}{tmpkey}'
     s3.download_file(bucket_name, key, download_path)
     return download_path
 
 
-def upload_file(bucket_name, file_path, key):
+def upload_file(bucket_name: str, file_path: str, key: str) -> bool:
     s3.upload_file(file_path, bucket_name, key)
     return True
--- src/service/convert.py
+++ src/service/convert.py
@@ -1,12 +1,12 @@
 from xhtml2pdf import pisa
 
 
-def convert_html_to_pdf(html_path):
+def convert_html_to_pdf(html_path: str) -> str:
     with open(html_path, "r", encoding='utf-8') as html_file:
         html_string = html_file.read()
 
     pdf_path = html_path.replace(".html", ".pdf")
-    
+
     with open(pdf_path, "wb") as pdf_file:
         pisa_status = pisa.CreatePDF(html_string, dest=pdf_file)
 
